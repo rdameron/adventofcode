414f46d5e73ce487e5321e39e6eb3f70d35df467
rdameron <ron.dameron@gmail.com>
2015-12-09 10:33:06 -0500

    Day 1 Puzzle 1

---
+++ AdventOfCodeD1P1.ps1
@@ -0,0 +1,4 @@
+﻿$floors = get-content 'C:\Users\rdameron\Downloads\AdventofCodeDay1P1.txt'
+$up = ([regex]::Matches($floors, "\(" )).count
+$down = ([regex]::Matches($floors, "\)" )).count
+$up - $down
