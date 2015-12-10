7b56547f48d3d72845dcb2c09511887c9606e3f9
rdameron <ron.dameron@gmail.com>
2015-12-10 15:30:42 -0500

    Advent of Code Day 1 Puzzle 2
    Powershell to answer AoC day 1 puzzle 2

---
+++ AdventOfCodeD1P2.ps1
@@ -0,0 +1,15 @@
+﻿$floors = get-content 'C:\Users\rdameron\Downloads\AdventofCodeDay1P1.txt'
+$floor = $floors.ToCharArray() 
+$r = 1
+$fl = 0
+foreach($f in $floor)
+{ 
+    if ($f -eq '(')
+      {$fl++}
+    else
+      {$fl--}
+    
+    if ($fl -eq -1) {$r; $fl; break;}
+    else 
+      {$r++}
+}
