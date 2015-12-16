2a1eccb079102becf4c6f710c1dbed7eddf48b02
rdameron <ron.dameron@gmail.com>
2015-12-15 21:26:11 -0500

    Advent of Code Day 2 Puzzle 2

---
+++ AoCd3p1.ps1
@@ -0,0 +1,35 @@
+﻿$in = get-content 'C:\Users\rdameron\Downloads\AoC_D3Input.txt' #| select -First 10
+$in = $in.ToCharArray()
+
+$x=1
+$y=1
+#$px=1
+#$py=1
+
+$Houses=1
+
+foreach($d in $in)
+{ 
+    switch ($d) {
+    '>' {$x++}
+    '<' {$x--}
+    '^' {$y++}
+    'v' {$y--}
+    }
+
+    #if ($x -ne $px) or ($y -ne $py) 
+        #{$Houses++}
+
+    #save positions in array or hashtable
+    #check if position is in array, if not save it. Use contains
+    #when done, count elements in hashtable? 
+
+    @{ X = '1'; Y = '1' }
+
+    
+}
+
+
+
+
+
