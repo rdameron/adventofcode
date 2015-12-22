33cc0a6fbbd69b227eeb40c12016790a9b7a7a1c
rdameron <ron.dameron@gmail.com>
2015-12-22 09:30:14 -0500

    Advent of Code Day 3 Puzzle 1
    Day 3 Puzzle 1

---
+++ Aocd3p1.ps1
@@ -0,0 +1,28 @@
+﻿$x=1
+$y=1
+
+$HousesVisited += @("1,1")
+
+﻿$in = get-content 'C:\Users\rdameron\Downloads\AoC_D3Input.txt' #| select -First 10
+$in = $in.ToCharArray()
+
+foreach($d in $in)
+{ 
+    switch ($d) {
+    '>' {$x++}
+    '<' {$x--}
+    '^' {$y++}
+    'v' {$y--}
+    }
+
+    if ($HousesVisited -notcontains "$x,$y") {
+            $HousesVisited += @("$x,$y")
+        }
+}
+
+#Unique house visits
+($HousesVisited | Get-Unique).Count
+
+
+
+
