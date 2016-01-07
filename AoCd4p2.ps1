dabe028d88c7ac06227a2f800272d47be0cdbd12
rdameron <ron.dameron@gmail.com>
2016-01-07 09:36:37 -0500

    Advent of Code Day 2 Puzzle 2
    This is basically the same code as Puzzle 1 with only a change in the
    exit condition for the DO WHILE to find an MD5 hash with SIX leading
    zeroes instead of five. It's not perfect it loops one extra time before
    exiting. This ran a lot longer than the Puzzle 1 script but did get me
    the correct answer.

---
+++ AoCd4p2.ps1
@@ -0,0 +1,34 @@
+﻿function Hash($textToHash)
+{
+    $hasher = new-object System.Security.Cryptography.MD5CryptoServiceProvider
+    $toHash = [System.Text.Encoding]::UTF8.GetBytes($textToHash)
+    $hashByteArray = $hasher.ComputeHash($toHash)
+    foreach($byte in $hashByteArray)
+    {
+         $res += $byte.ToString("x2")
+    }
+    return $res;
+}
+
+$secret = 'ckczppom'
+$a = 1
+
+Do
+
+    {
+
+     "Starting Loop $a"
+        
+
+     $h = hash($secret + $a) 
+
+     $a++
+
+     $h
+
+     "Now `$a is $a"
+
+    } Until ($h -like '000000*')
+
+    $h
+
